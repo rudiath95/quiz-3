655c65b6793ec7427efba6a7240c416deae96ad6
Rudiath Rizky Adi Nugraha <rudiath.r@gmail.com>
2023-02-20 07:13:06 +0700

    SELESAI

--- repository/books.go
+++ repository/books.go
@@ -21,7 +21,7 @@ func GetAllBooks(db *sql.DB) (err error, results []structs.Books) {
 	for rows.Next() {
 		var books = structs.Books{}
 
-		err = rows.Scan(&books.ID, &books.Title, &books.Created_at, &books.Updated_at)
+		err = rows.Scan(&books.ID, &books.Title, &books.Description, &books.Image, &books.Price, &books.TotalPage, &books.Thickness, &books.ReleaseYear, &books.Created_at, &books.Updated_at, &books.CategoryID)
 		if err != nil {
 			panic(err)
 		}
