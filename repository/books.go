f4a001b1f0f59eb16dfebc8b0f5a092e86a68c8d
Rudiath Rizky Adi Nugraha <rudiath.r@gmail.com>
2023-02-20 07:34:54 +0700

    fixing again

--- repository/books.go
+++ repository/books.go
@@ -3,9 +3,12 @@ package repository
 import (
 	"database/sql"
 	"log"
+	"net/http"
 	"quiz3/structs"
 	"regexp"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 func GetAllBooks(db *sql.DB) (err error, results []structs.Books) {
@@ -31,7 +34,7 @@ func GetAllBooks(db *sql.DB) (err error, results []structs.Books) {
 	return
 }
 
-func InsertBooks(db *sql.DB, books structs.Books) (err error) {
+func InsertBooks(db *sql.DB, books structs.Books c *gin.Context) (err error) {
 
 	sql := "INSERT INTO books (id, title, description, image_url, price, total_page, thickness, release_year, updated_at, category_id) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)"
 
@@ -58,6 +61,8 @@ func InsertBooks(db *sql.DB, books structs.Books) (err error) {
 	var regex, _ = regexp.Compile(`([^\s]+(\.(?i)(jpe?g|png|gif|bmp|webp))$)`)
 
 	if books.ReleaseYear < 1980 && regex.MatchString(books.Image) {
+		c.String(http.StatusOK, "ReleaseYear tidak boleh kurang dari 1980")
+
 		panic("ReleaseYear tidak boleh kurang dari 1980")
 	} else if books.ReleaseYear > 2021 && regex.MatchString(books.Image) {
 		panic("ReleaseYear tidak boleh lebih dari 2021")
